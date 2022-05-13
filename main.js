01c4f96211068ec61fc31da34f2bd88ddb5a21e4
krishna404 <agarwalbalkrishnaindia@gmail.com>
2022-05-13 19:50:12 +0530

    Work Ex partially done

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-var that = this;
-setTimeout(function () {
-  that.style.opacity = 1;
-}, 500);
+window.addEventListener("DOMContentLoaded", function(){
+  let start = 0;
+
+  if (start ===0) {
+    start++;
+    let workexElements = document.getElementsByClassName('workitem');
+    workexclick(workexElements[0]);
+  };
+})
+
+
+function workexclick(clickedElement){
+  // console.log(clickedElement);
+  let count = clickedElement.getAttribute('count');
+  // console.log(count);
+  let workmenuElements = document.getElementsByClassName('workitem');
+  let workdeetsElements = document.getElementsByClassName('workdeetsitem');
+
+  for(let i=0; i<workdeetsElements.length; i++){
+    if (i == count){
+      workmenuElements[i].style.color = '#7CAE7A'
+      workmenuElements[i].style['border-left']= '2px solid #7CAE7A';
+      workdeetsElements[i].style.display = 'block';      
+    } else {
+      workmenuElements[i].style.color = '#003249'
+      workmenuElements[i].style['border-left']= '2px solid #839073';
+      workdeetsElements[i].style.display = 'none';
+    }
+  }
+}
